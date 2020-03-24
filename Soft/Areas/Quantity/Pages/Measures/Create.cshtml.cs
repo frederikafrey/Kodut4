0cdae4fb35fa2a5c34f220bcf4d3fdb1cc2b29a5
Frederika Frey <frfrey@ttu.ee>
2020-03-24 11:57:41 +0200

    37 done

--- Soft/Areas/Quantity/Pages/Measures/Create.cshtml.cs
+++ Soft/Areas/Quantity/Pages/Measures/Create.cshtml.cs
@@ -11,12 +11,20 @@ namespace Abc.Soft.Areas.Quantity.Pages.Measures
         {
 
         }
-        public IActionResult OnGet() => Page();
 
-        public async Task<IActionResult> OnPostAsync()
+        public IActionResult OnGet(string fixedFilter, string fixedValue)
         {
+            FixedFilter = fixedFilter;
+            FixedValue = fixedValue;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string fixedFilter, string fixedValue)
+        {
+            FixedFilter = fixedFilter;
+            FixedValue = fixedValue;
             if (!await addObject()) return Page();
-            return RedirectToPage("./Index");
+            return Redirect($"/Quantity/Measures/Index?fixedFilter={FixedFilter}&fixedValue={FixedValue}");
         }
     }
 }
--- Soft/Areas/Quantity/Pages/Units/Create.cshtml.cs
+++ Soft/Areas/Quantity/Pages/Units/Create.cshtml.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Abc.Domain.Quantity;
 using Abc.Pages.Quantity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Abc.Soft.Areas.Quantity.Pages.Units
 {
@@ -11,12 +9,19 @@ namespace Abc.Soft.Areas.Quantity.Pages.Units
     {
         public CreateModel(IUnitsRepository r, IMeasuresRepository m) : base(r, m) { }
 
-        public IActionResult OnGet() => Page();
-
-        public async Task<IActionResult> OnPostAsync()
+        public IActionResult OnGet(string fixedFilter, string fixedValue)
         {
+            FixedFilter = fixedFilter;
+            FixedValue = fixedValue;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string fixedFilter, string fixedValue)
+        {
+            FixedFilter = fixedFilter;
+            FixedValue = fixedValue;
             if (!await addObject()) return Page();
-            return RedirectToPage("./Index");
+            return Redirect($"/Quantity/Units/Index?fixedFilter={FixedFilter}&fixedValue={FixedValue}");
         }
     }
 }
