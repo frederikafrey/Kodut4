e27135f1b554bc8fad5da0708966eafdb3573baa
Frederika Frey <frfrey@ttu.ee>
2020-03-25 17:17:38 +0200

    40 done

--- Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
+++ Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
@@ -1,4 +1,8 @@
-﻿using Abc.Pages.Extensions;
+﻿using System.Collections.Generic;
+using System.Threading;
+using Abc.Facade.Quantity;
+using Abc.Pages.Extensions;
+using Microsoft.AspNetCore.Html;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Abc.Tests.Pages.Extensions
@@ -12,8 +16,16 @@ namespace Abc.Tests.Pages.Extensions
         [TestMethod]
         public void EditControlsForTest()
         {
-            Assert.Inconclusive();
+            var obj = new htmlHelperMock<UnitView>().EditControlsFor(x => x.MeasureId);
+            Assert.IsInstanceOfType(obj, typeof(HtmlContentBuilder));
         }
 
+        [TestMethod]
+        public void HtmlStringsTest()
+        {
+            var expected = new List<string> { "<div", "LabelFor", "EditorFor", "ValidationMessageFor", "</div>"};
+            var actual = EditControlsForHtmlExtension.htmlString(new htmlHelperMock<MeasureView>(), x => x.Name);
+            TestHtml.Strings(actual, expected);
+        }
     }
 }
