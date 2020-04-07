f78d965c98ff6489a441937e55366e2537d352c9
Frederika Frey <frfrey@ttu.ee>
2020-04-07 13:34:51 +0300

    53 done

--- Tests/Infra/Quantity/MeasuresRepositoryTests.cs
+++ Tests/Infra/Quantity/MeasuresRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Abc.Aids;
 using Abc.Data.Quantity;
 using Abc.Domain.Quantity;
@@ -12,6 +13,9 @@ namespace Abc.Tests.Infra.Quantity
     [TestClass]
     public class MeasuresRepositoryTests : RepositoryTests<MeasuresRepository, Measure, MeasureData>
     {
+        private QuantityDbContext db;
+        private int count;
+
         [TestInitialize]
         public override void TestInitialize()
         {
@@ -19,8 +23,18 @@ namespace Abc.Tests.Infra.Quantity
             var options = new DbContextOptionsBuilder<QuantityDbContext>()
                 .UseInMemoryDatabase("TestDb")
                 .Options;
-            var c = new QuantityDbContext(options);
-            obj = new MeasuresRepository(c);
+            db = new QuantityDbContext(options);
+            obj = new MeasuresRepository(db);
+            foreach (var p in db.Measures)
+                db.Entry(p).State = EntityState.Deleted;
+            count = GetRandom.UInt8(20, 40);
+            addItems();
+        }
+
+        private void addItems()
+        {
+            for (var i = 0; i < count; i++)
+                obj.Add(new Measure(GetRandom.Object<MeasureData>())).GetAwaiter();
         }
 
         protected override Type getBaseType()
@@ -30,7 +44,9 @@ namespace Abc.Tests.Infra.Quantity
 
         protected override void testGetList()
         {
-            Assert.Inconclusive();
+            obj.PageIndex = GetRandom.Int32(2, obj.TotalPages - 1);
+            var l = obj.Get().GetAwaiter().GetResult();
+            Assert.AreEqual(obj.PageSize, l.Count);
         }
 
         protected override string getId(MeasureData d) => d.Id;
